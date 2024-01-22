b48ab5088e8eafb2a8a0f7de811f54ad2dcdce10
Jordyn Kuhn <jkuhn2472@mail.kvcc.edu>
2024-01-22 16:50:57 -0500

    Add tests to verify pages work correctly Add files required to delopy on heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(6t9@9@wa7or$n4gwg_tb3_mr84gl$+yk-p3vm&*4#x&r=(ky#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomepageTest(SimpleTestCase):
+    """Test the home Page"""
+    def test_url_exists_at_correct_location(self):
+        """test url exists at correct location"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """test url available by name"""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        """Test template name is correct"""
+        response=self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        """Test template content"""
+        response=self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+
+class AboutpageTest(SimpleTestCase):
+    """Test the about Page"""
+    def test_url_exists_at_correct_location(self):
+        """test url exists at correct location"""
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """test url available by name"""
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """test url available by name"""
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        """Test template name is correct"""
+        response=self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):
+        """Test template content"""
+        response=self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About Page</h1>")
+  
